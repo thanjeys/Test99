a5afff1a42fd5bfa0d7ad0e2b7d79a4caf8bb8ba
Thananjeyan <thanjeys@gmail.com>
2023-05-24 13:23:06 +0530

    first commit

---
+++ hello.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello world @ github</title>
+</head>
+<body>
+    <h1>Hello, PHP </h1>
+    <p>Welcome to github website. This is my first github document. Thank you </p>
+
+    <h1>Testing with New PC</h1>
+    <h1>Thana  with New PC</h1>
+</body>
+</html>
